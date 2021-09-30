0589e0e6b99ddcb969bb0b93bde4506dd23a3194
Thiago Rocha <thiagord2016@gmail.com>
2021-09-30 20:12:24 -0300

    Passando de "JS-only" para +Html/Css

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// Baseado no vídeo do Filipe Deschamps: https://www.youtube.com/watch?v=dW08ABBu2SY
-var imgBlack = document.createElement('img');
-imgBlack.src = 'https://sportbuzz.uol.com.br/media/_versions/douglascosta_82934045_116411013185365_8270789263511275541_n_widexl.jpg';
-document.body.appendChild(imgBlack)
-// ===
-imgBlack.style.width = '100px';
-imgBlack.style.height = '100px';
-imgBlack.style.borderRadius = '50%';
-// ===
-imgBlack.style.position = 'absolute';
-imgBlack.style.top = '50%';
-imgBlack.style.marginTop = '-50px';
-// ===
+// Baseado no vídeo do Filipe Deschamps: https://www.youtube.com/watch?v=dW08ABBu2SY    
+var imgBlack = document.getElementById("imgBlack");
+
 var x = 0;
 var xDirection = 1;
 var xSpeed = 10;
+
 function animateImgBlack() {
     x += xDirection * xSpeed;
     if (x > window.innerWidth - 100) {
@@ -27,27 +18,17 @@ function animateImgBlack() {
     window.requestAnimationFrame(animateImgBlack);
 }
 animateImgBlack();
-// ===
-document.body.style.overflow = 'hidden';
-// ===
-document.body.style.backgroundColor = 'aquamarine';
-// ===
-var imgRed = document.createElement('img');
-imgRed.src = 'https://st.depositphotos.com/1011767/1748/i/600/depositphotos_17486139-stock-photo-red-background.jpg';
-document.body.appendChild(imgRed);
-// ===
-imgRed.style.width = '80px';
-imgRed.style.height = '80px';
-imgRed.style.borderRadius = '50%';
-// ===
-imgRed.style.position = 'absolute';
-// ===
+
+
+var imgRed = document.getElementById("imgRed");
+
 var redX = Math.random() * (window.innerWidth - imgRed.offsetWidth);
 var redY = Math.random() * (window.innerHeight - imgRed.offsetHeight);
 var redXdirection = 1;
 var redYdirection = 1;
 var redXspeed = xSpeed * 1.1;
 var redYspeed = xSpeed * 1.1;
+
 function animateImgRed() {
     redX += redXdirection * redXspeed;
     redY += redYdirection * redYspeed;
